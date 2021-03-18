260ad9c07cc99ff1c88df4483bc88023e3cf67e4
Sho Ya Voorthuyzen <shoya@kali.local>
2021-03-18 00:26:16 -0400

    upload domain_info_gather script

---
+++ domain_info_gather.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+while true
+	do
+	echo "Enter the domain(s) to lookup: "
+
+	read domains
+
+	[[ $domains == "exit" ]] && exit 0
+
+	for domain in $domains
+		do
+		rx='([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
+		
+		if [[ $domain =~ ^$rx\.$rx\.$rx\.$rx$ ]]; then
+			echo "whois ${domain}"
+			whois $domain | awk '/NetRange: /,/OrgTechRef: /'
+		else
+			echo "whois ${domain}"
+			whois $domain | grep -e 'Domain Name:' -e 'Name Server:' -e 'Registrant ' -e 'Admin ' -e 'Tech '
+			printf "\ndig ${domain}\n"
+			dig $domain | awk '/ANSWER SECTION:/,/Query time: /' | sed '$d' | sed '1d'
+		fi
+		echo "nslookup ${domain}"
+		nslookup $domain
+		done
+
+done
